077fdcc8121e7df6623ad9a298f7598b5850daf4
MarekITPro <MarekITPro@users.noreply.github.com>
2020-12-11 09:54:21 +0000

    Update super.sh

--- super.sh
+++ super.sh
@@ -5,6 +5,16 @@ if [[ $# -ne 2 ]]; then
     exit 2
 fi
 
+# wait for disk to come online (max 50 minutes)
+counter=0
+while [ ! -e /dev/sdc ]; do
+    sleep 1m
+    counter=$((counter + 1))
+    if [ $counter -ge 50 ]; then
+        exit
+    fi
+done
+
 echo "partition data disk"
 sudo parted /dev/disk/azure/scsi1/lun0 mklabel gpt
 sudo parted -a opt /dev/disk/azure/scsi1/lun0 mkpart primary ext4 0% 100%
@@ -30,3 +40,6 @@ sudo wget -O /datadrive/tools/azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v1
 echo "extract azcopy"
 sudo tar -xf /datadrive/tools/azcopy_v10.tar.gz --strip-components=1 -C /datadrive/tools
 sudo chmod +x /datadrive/tools/azcopy
+
+echo "install AzureCLI"
+sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
